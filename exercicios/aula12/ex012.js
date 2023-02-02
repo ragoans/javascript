f1c584820661f976be539c80eb643411befab3ee
ragoans <rafaelgoulart12@gmail.com>
2023-02-02 08:16:48 -0400

    Aula 12 - Condições Parte 2

---
+++ exercicios/aula12/ex012.js
@@ -0,0 +1,11 @@
+// Condição aninhada
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora <= 12) {
+    console.log('Bom Dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
