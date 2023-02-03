c485e1eb8a1841aa46b564b992d540758cc2a6ee
ragoans <rafaelgoulart12@gmail.com>
2023-02-03 11:39:29 -0400

    Aula 13 - Do e While

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,27 @@
+var c = 1 //variável contadora
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1 - contador
+} while (c <= 6)
+
+
+
+/* 
+var c = 1 //variável contadora
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1 - contador
+}
+*/
+
+
+
+
+/*
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
