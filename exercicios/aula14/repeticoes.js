c31c7e0c80623c411590400ab472f51a18121344
ragoans <rafaelgoulart12@gmail.com>
2023-02-05 22:04:02 -0400

    Aula 14

---
+++ exercicios/aula14/repeticoes.js
@@ -0,0 +1,17 @@
+/*
+Modelo While
+------------------------------
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+console.log('Vai começar...')
+for (var c = 1; c <= 4; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')
+
